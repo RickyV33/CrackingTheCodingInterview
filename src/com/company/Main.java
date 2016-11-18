a17ac5637e6800c838e6b1faebb0f965ab96610c
Ricky Valencia <3D.RickyV@gmail.com>
2016-11-17 16:35:25 -0800

    add URLify problem 1.3 of strings/lists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,17 @@ public class Main {
 //        System.out.println(u.isUniqueTwo("abcdefg"));
 //        System.out.println(u.isUniqueTwo("abcdefa"));
 //        System.out.println(u.isUniqueTwo("a1c"));
-        Permutation p = new Permutation();
-        System.out.println(p.isPermutation("abc", "bac"));
-        System.out.println(p.isPermutation("bbbac", "abcbb"));
-        System.out.println(p.isPermutation("abc", "c"));
-        System.out.println(p.isPermutation("abcdf", "kdfjd"));
+//        Permutation p = new Permutation();
+//        System.out.println(p.isPermutation("abc", "bac"));
+//        System.out.println(p.isPermutation("bbbac", "abcbb"));
+//        System.out.println(p.isPermutation("abc", "c"));
+//        System.out.println(p.isPermutation("abcdf", "kdfjd"));
+        URLify u = new URLify();
+        System.out.println(u.URLify("this is a test      ".toCharArray(), 20));
+        System.out.println(u.URLify("   ".toCharArray(), 3));
+        System.out.println(u.URLify("".toCharArray(), 0));
+        System.out.println(u.URLify("a".toCharArray(), 1));
+        System.out.println(u.URLify("this test  ".toCharArray(), 11));
 
     }
 }
---
+++ src/com/company/URLify.java
@@ -0,0 +1,36 @@
+package com.company;
+
+/**
+ * Created by Slick on 11/17/16.
+ */
+public class URLify {
+    public char[] URLify(char[] url, int len) {
+        if (len == 0 || url == null) {
+            return url;
+        }
+        for (int i = 0; i < len; ++i) {
+            if (url[i] == ' ') {
+                url = shiftTwice(url, i + 1);
+                url = replaceSpace(url, i);
+            }
+        }
+        return url;
+    }
+
+    private char[] replaceSpace(char[] url, int start) {
+        char[] space = "%20".toCharArray();
+        for (int i = 0; i < 3; ++i) {
+            url[start+i] = space[i];
+        }
+        return url;
+    }
+
+    private char[] shiftTwice(char[] url, int start) {
+        for (int i = 0; i < 2; ++i) {
+            for(int k = url.length-1; k > start; --k) {
+                url[k] = url[k-1];
+            }
+        }
+        return url;
+    }
+}
