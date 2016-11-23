8f107f976a80b00d6f580598a291d6286912138f
Ricky Valencia <3D.RickyV@gmail.com>
2016-11-22 20:31:57 -0800

    add tests for Unique

--- tests/com.company/UniqueTest.java
+++ tests/com.company/UniqueTest.java
@@ -1,7 +1,39 @@
 package com.company;
 
-/**
- * Created by Slick on 11/22/16.
- */
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class UniqueTest {
+    private Unique unique = new Unique();
+    @Test
+    public void testIsUniqueWithUniqueString() {
+        assertTrue(unique.isUnique("abcdefg"));
+    }
+
+    @Test
+    public void testIsUniqueWithoutUniqueString() {
+        assertFalse(unique.isUnique("abcdaefg"));
+    }
+
+    @Test
+    public void testIsUniqueWithoutCaseSensitiveCharacters() {
+        assertFalse(unique.isUnique("abcdA"));
+    }
+
+    @Test
+    public void testIsUniqueWhenNull() {
+        assertFalse(unique.isUnique(null));
+    }
+
+    @Test
+    public void testIsUniqueWithEmptyString() {
+        assertFalse(unique.isUnique(""));
+    }
+
+    @Test
+    public void testIsUniqueWithSingleCharacter() {
+        assertTrue(unique.isUnique("a"));
+    }
 }
