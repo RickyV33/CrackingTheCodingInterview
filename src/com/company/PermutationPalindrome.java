cc4a24178555647b610a50f039b1142c4d6c144f
Ricky Valencia <3D.RickyV@gmail.com>
2016-11-22 20:32:15 -0800

    add inital Permutation Palindrome file

--- src/com/company/PermutationPalindrome.java
+++ src/com/company/PermutationPalindrome.java
@@ -1,27 +1,40 @@
 package com.company;
 
-public class Palindrome {
+public class PermutationPalindrome {
 
-    public boolean isPalindrome(String s) {
-        if (s == null) {
-            return false;
-        }
-        s = s.toLowerCase();
-        int len = s.length();
-        if (len == 0) {
-            return false;
-        }
-        for (int i = 0, k = len - 1; i < len && k >= 0; ++i, --k) {
-            while (s.charAt(i) == ' ') {
-                ++i;
-            }
-            while (s.charAt(k) == ' ') {
-                --k;
-            }
-            if (s.charAt(i) != s.charAt(k)) {
-                return false;
-            }
-        }
+//    public boolean isPalindrome(String s) {
+//        if (s == null) {
+//            return false;
+//        }
+//        s = s.toLowerCase();
+//        int len = s.length();
+//        if (len == 0) {
+//            return false;
+//        }
+//        for (int i = 0, k = len - 1; i < len && k >= 0; ++i, --k) {
+//            while (s.charAt(i) == ' ') {
+//                ++i;
+//            }
+//            while (s.charAt(k) == ' ') {
+//                --k;
+//            }
+//            if (s.charAt(i) != s.charAt(k)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+    /*
+    Initial Thoughts:
+    Can whitespace be ignored?
+    Can puncuation be ignored?
+    Can numbers be ignored?
+    Is it safe to assume only letters are applicable?
+    Is it case sensitive? - let's assume it is.
+    EDGE CASES: empty string, null string.
+    */
+    public boolean isPermutationPalindrome(String s) {
         return true;
     }
 }
